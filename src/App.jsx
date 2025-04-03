2c0c8f64b5cafa5e5d546f7598bb6bb62a9d3c24
bakhitawanjiru <wanjirugithiori@gmail.com>
2025-04-03 12:27:57 +0300

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 
 import './App.css'
+import Courses from './components/Courses'
 
 function App() {
  
---
+++ src/components/Courses.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useState } from 'react'
+
+function Courses() {
+  const [course,setCourse]=useState('')
+  return (
+    <div>
+      <div>
+        <p className='Title'>We Offer The Following Courses</p>
+      </div>
+      <div className='container'>
+
+      </div>
+        
+    </div>
+  )
+}
+
+export default Courses
